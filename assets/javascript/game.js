fc001e67d1b457beaa692397619908ca6300527f
Liam Condon <condonliamt@gmail.com>
2019-02-28 21:10:31 -0800

    restructured js towards more of a function-oriented build that caters to three primary/parent functions; also added some additional animations and stylings

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,36 @@
+// Queue landing screen when document has rendered:
+
+// $(document).ready(landingScreen);
+// function landingScreen()
+
+// Global variables declared:
+var characters = {
+    jonSnow: {hp: 100, ap: 50, cp: 100},
+    cersei: {hp: 100, ap: 150, cp: 100},
+    dany: {hp: 100, ap: 150, cp: 100},
+    nightKing: {hp: 100, ap: 50, cp: 100}
+}
+
+var charChosen = false;
+var opponentChosen = false;
+var jonSnowChosen = false;
+var cerseiChosen = false;
+var danyChosen = false;
+var nightKingChosen = false;
+var gameInitiated = false;
+var charID;
+var charDetach;
+// Global buttons declared:
+$("#main-button").click(characterScreen);
 
 
-// function animateCSS(element, animationName, callback) {
-//     const node = document.querySelector(element)
-//     node.classList.add('animated', animationName)
+    $(".enemy-button").click(battleScreen);
 
-//     function handleAnimationEnd() {
-//         node.classList.remove('animated', animationName)
-//         node.removeEventListener('animationend', handleAnimationEnd)
-
-//         if (typeof callback === 'function') callback()
-//     }
-
-//     node.addEventListener('animationend', handleAnimationEnd)
-// }
-
-var jonSnow = {
-    hp: 100,
-    ap: 50,
-    cp: 100
-};
-
-var cersei = {
-    hp: 100,
-    ap: 150,
-    cp: 100
-};
-
-var dany = {
-    hp: 100,
-    ap: 150,
-    cp: 100
-};
+// Primary functions:
 
 
-    $("#main-button").on("click", function(){
+
+    function characterScreen() {
       $("#large-logo").animate({height: '75px'});
       $('#main-button').fadeOut();
       $("#left-characters").show();
@@ -43,19 +40,63 @@ var dany = {
       $("#landing-page").animate({"padding-top": "-=4%"}, 200);
       $("#opener ").fadeOut();
       setTimeout(function(){ $('#choose-char').fadeIn() }, 350);
-    });
+      $(".character-container").click(function(){ 
+        if (!charChosen) {
+            charID = $(this)
+            charChosen = true;
+            opponentScreen();
+        };
+      });
+    };
 
-    $("#jonSnow").on("click", function() {
-        $("#jonSnow").addClass('animated bounce').one('animationend', chooseJon());
-    });
 
-    function chooseJohn() {
-        var charChoice = jonSnow;
-        var defender = cersei;
-        var firstBystander = dany;
-        var secondBystander = nightKing;
+    function opponentScreen() {
+        $(".character-container").not($(charID)).fadeOut('fast'); // fade out every other div
+            $(charID).addClass('animated bounce').one('animationend', function() {
+                $(charID).fadeOut();
+                $("#last.word").fadeOut();
+                setTimeout(function(){ $(".character-container").not($(charID)).fadeIn()}, 500);
+                setTimeout(function(){ $("#last-word").text("Opponent")}, 100);
+            })
+        
+        // $(".character-container").not($(charID)).fadeIn();
+        // $("#landing-page").append($(".character-container").not($(charID)));
+        // 
+        // charChoice = characters.jonSnow;
+        // defender = cersei;
+        // firstBystander = dany;
+        // secondBystander = nightKing;
+    
+    
 
-        var detatchCharChoice = 
-        $("#landing-page").fadeOut();
-        $("#cersei").appendTo()
+  
     }
+    
+    function battleScreen() {
+      // var detatchCharChoice = 
+      $("#landing-page").fadeOut();
+      $("#battlefield").fadeIn();
+      $("#cersei").appendTo("#defender-area");
+      $("#dany").appendTo("#first-bystander");
+      $("#nightKing").appendTo("#second-bystander");
+      $("#jonSnow").appendTo("#player-area");
+      $("#attack-button").fadeIn();
+      $("#vs").fadeIn();
+      // $("#player-stats").append(")
+    }
+
+    
+        // $("#jonSnow").addClass('animated bounce').one('animationend', function(){
+        //     // if ((!jonSnowChosen) && (!gameInitiated)) {
+        //     //     var charChoice = jonSnow;
+        //     // var defender = cersei;
+        //     // var firstBystander = dany;
+        //     // var secondBystander = nightKing;
+        //     // }
+
+
+            
+        // });
+
+    // function chooseJohn() {
+    // }
